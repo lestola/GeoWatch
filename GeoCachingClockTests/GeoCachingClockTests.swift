a67b3ed6c864903696013592df42931ffa62fd84
Marko Lehtola <marko@marko-macbook-pro.local>
2017-05-14 14:50:12 +0300

    Initial Commit

---
+++ GeoCachingClockTests/GeoCachingClockTests.swift
@@ -0,0 +1,36 @@
+//
+//  GeoCachingClockTests.swift
+//  GeoCachingClockTests
+//
+//  Created by Marko Lehtola on 14.5.2017.
+//  Copyright © 2017 Lestola Industries. All rights reserved.
+//
+
+import XCTest
+@testable import GeoCachingClock
+
+class GeoCachingClockTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
